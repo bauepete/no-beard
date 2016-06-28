8f5dcb8025a2c0b4ed6c0fc8e618f2e74639f2ec
Peter Bauer <p.bauer@htl-leonding.ac.at>
2016-06-28 18:01:53 +0200

    Removed @BeforeClass etc.

--- NoBeardProject/test/symboltable/SymListEntryTest.java
+++ NoBeardProject/test/symboltable/SymListEntryTest.java
@@ -5,9 +5,7 @@
 package symboltable;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import symboltable.Operand.Kind;
@@ -22,14 +20,6 @@ public class SymListEntryTest {
     public SymListEntryTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
     @Before
     public void setUp() {
     }
