22546bc9fe256b84c9e9853efdf9bbf73dc928a3
Peter Bauer <p.bauer@htl-leonding.ac.at>
2016-07-19 13:50:50 +0200

    First tests for syntactically parsing simple variable declarations.

--- NoBeardProject/src/parser/VariableDeclarationParser.java
+++ NoBeardProject/src/parser/VariableDeclarationParser.java
@@ -1,6 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright ©2012 to 2016. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
+ * Department of Informatics and Media Technique, HTBLA Leonding, 
+ * Limesstr. 12 - 14, 4060 Leonding, AUSTRIA. All Rights Reserved. Permission
+ * to use, copy, modify, and distribute this software and its documentation
+ * for educational, research, and not-for-profit purposes, without fee and
+ * without a signed licensing agreement, is hereby granted, provided that the
+ * above copyright notice, this paragraph and the following two paragraphs
+ * appear in all copies, modifications, and distributions. Contact the Head of
+ * Informatics and Media Technique, HTBLA Leonding, Limesstr. 12 - 14,
+ * 4060 Leonding, Austria, for commercial licensing opportunities.
+ * 
+ * IN NO EVENT SHALL HTBLA LEONDING BE LIABLE TO ANY PARTY FOR DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+ * EVEN IF HTBLA LEONDING HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HTBLA LEONDING SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
+ * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
 package parser;
 
@@ -25,7 +44,36 @@ public class VariableDeclarationParser extends Parser {
     private ElementType elemType;
 
     VariableDeclarationParser(Scanner s, SymListManager sym, CodeGenerator c, ErrorHandler eh) {
-        super(s, sym, c, eh);
+        super();
+    }
+
+    public VariableDeclarationParser() {
+    }
+
+    @Override
+    protected void parseSpecificPart() {
+        assertThatCurrentSymbolIsOf(Symbol.INT, Symbol.CHAR, Symbol.BOOL);
+        parseSymbol(scanner.getCurrentToken().getSy());
+        parseSymbol(Symbol.IDENTIFIER);
+        parseSymbol(Symbol.SEMICOLON);
+    }
+
+    /**
+     * Checks whether the current symbol is one of the symbols given as arguments.
+     * @param symbol One of the possible symbols.
+     * @param furtherSymbols Further possible symbols.
+     */
+    protected void assertThatCurrentSymbolIsOf(Symbol symbol, Symbol... furtherSymbols) {
+        if (!parsingWasSuccessful()) return;
+        
+        Symbol currentSymbol = scanner.getCurrentToken().getSy();
+        boolean properSymbolFound = currentSymbol == symbol;
+        
+        setWasSuccessful(properSymbolFound);
+        for (Symbol sy : furtherSymbols) {
+            properSymbolFound = properSymbolFound || currentSymbol == sy;
+            setWasSuccessful(properSymbolFound);
+        }
     }
 
     @Override
@@ -72,7 +120,6 @@ public class VariableDeclarationParser extends Parser {
             sym.newVar(name, elemType);
             // endsem
         }
-
 
         if (scanner.getCurrentToken().getSy() == Symbol.ASSIGN) {
             // sem
@@ -134,10 +181,5 @@ public class VariableDeclarationParser extends Parser {
         }
         scanner.nextToken();
         return true;
-    }
-
-    @Override
-    protected void parseSpecificPart() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
---
+++ NoBeardProject/test/parser/syntax/VariableDeclarationParserTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright ©2015, 2016. Created by P. Bauer (p.bauer@htl-leonding.ac.at),
+ * Department of Informatics and Media Technique, HTBLA Leonding, 
+ * Limesstr. 12 - 14, 4060 Leonding, AUSTRIA. All Rights Reserved. Permission
+ * to use, copy, modify, and distribute this software and its documentation
+ * for educational, research, and not-for-profit purposes, without fee and
+ * without a signed licensing agreement, is hereby granted, provided that the
+ * above copyright notice, this paragraph and the following two paragraphs
+ * appear in all copies, modifications, and distributions. Contact the Head of
+ * Informatics and Media Technique, HTBLA Leonding, Limesstr. 12 - 14,
+ * 4060 Leonding, Austria, for commercial licensing opportunities.
+ * 
+ * IN NO EVENT SHALL HTBLA LEONDING BE LIABLE TO ANY PARTY FOR DIRECT,
+ * INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST
+ * PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+ * EVEN IF HTBLA LEONDING HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HTBLA LEONDING SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY,
+ * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
+ * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ */
+package parser.syntax;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import parser.ParserFactory;
+import parser.VariableDeclarationParser;
+import parser.general.ParserTestSetup;
+import scanner.Scanner;
+
+/**
+ *
+ * @author P. Bauer (p.bauer@htl-leonding.ac.at)
+ */
+public class VariableDeclarationParserTest extends ParserTestSetup {
+    
+    public VariableDeclarationParserTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test simple integer declaration.
+     */
+    @Test
+    public void testSimpleInt() {
+        VariableDeclarationParser instance = setupTestObjectsAndParser("int x;");
+        boolean expResult = true;
+        boolean result = instance.parse();
+        assertEquals(expResult, result);
+        assertEquals(Scanner.Symbol.EOFSY, getScanner().getCurrentToken().getSy());
+    }
+
+    private VariableDeclarationParser setupTestObjectsAndParser(final String srcLine) {
+        setupInfraStructure(srcLine);
+        VariableDeclarationParser instance = ParserFactory.create(VariableDeclarationParser.class);
+        return instance;
+    }
+
+    /**
+     * Test simple char declaration.
+     */
+    @Test
+    public void testSimpleChar() {
+        VariableDeclarationParser instance = setupTestObjectsAndParser("char x;");
+        assertTrue(instance.parse());
+        assertEquals(Scanner.Symbol.EOFSY, getScanner().getCurrentToken().getSy());
+    }
+    /**
+     * Test invalid variable declaration.
+     */
+    @Test
+    public void testInvalidDeclaration() {
+        VariableDeclarationParser instance = setupTestObjectsAndParser("x");
+        assertFalse(instance.parse());
+    }
+    
+    @Test
+    public void testSimpleBool() {
+        VariableDeclarationParser instance = setupTestObjectsAndParser("bool x;");
+        assertTrue(instance.parse());
+        assertEquals(Scanner.Symbol.EOFSY, getScanner().getCurrentToken().getSy());
+    }
+}
