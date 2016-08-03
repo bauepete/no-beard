1e5221c01d4aa139c500944af990390ab0adc486
Peter Bauer <p.bauer@htl-leonding.ac.at>
2016-08-03 14:38:48 +0200

    Thrown away useless NoBeardCompiler methods.

--- NoBeardProject/src/compiler/NoBeardCompiler.java
+++ NoBeardProject/src/compiler/NoBeardCompiler.java
@@ -23,18 +23,13 @@
  */
 package compiler;
 
-import error.ErrorHandler;
 import java.io.FileNotFoundException;
-import nbm.CodeGenerator;
 import parser.NoBeardParser;
 import parser.Parser;
 import parser.ParserFactory;
-import scanner.Scanner;
 import scanner.SrcFileReader;
 import scanner.SrcReader;
 import scanner.SrcStringReader;
-import symboltable.Operand;
-import symboltable.SymbolTable;
 
 /**
  *
@@ -77,47 +72,4 @@ public class NoBeardCompiler {
             parserl = ParserFactory.create(NoBeardParser.class);
         }
     }
-
-    private final Scanner scanner;
-    private final NoBeardParser parser;
-    private final SymbolTable sym;
-    private final CodeGenerator code;
-    private final ErrorHandler errorHandler;
-
-    public NoBeardCompiler(SrcReader srcReader) {
-
-        errorHandler = new ErrorHandler(srcReader);
-        scanner = new Scanner(srcReader, errorHandler);
-        code = new CodeGenerator(nbm.Nbm.getMAXPROG());
-        sym = new SymbolTable(scanner, errorHandler);
-        Operand.setSymListManager(sym);
-        Operand.setStringManager(scanner.getStringManager());
-        parser = new NoBeardParser(scanner, sym, code, errorHandler);
-        scanner.nextToken();
-    }
-
-    public CodeGenerator getCode() {
-        return code;
-    }
-
-    public NoBeardParser getParser() {
-        return parser;
-    }
-
-    public Scanner getScanner() {
-        return scanner;
-    }
-
-    public SymbolTable getSymListManager() {
-        return sym;
-    }
-
-    public byte[] getStringStorage() {
-        return scanner.getStringManager().getStringStorage();
-    }
-
-    public ErrorHandler getErrorHandler() {
-        return errorHandler;
-    }
-
 }
