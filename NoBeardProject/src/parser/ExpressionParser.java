c957027d20447d02bbadaa7c6b448714903ace61
Peter Bauer <p.bauer@htl-leonding.ac.at>
2016-08-03 17:26:19 +0200

    Removed unnecessary field.

--- NoBeardProject/src/parser/ExpressionParser.java
+++ NoBeardProject/src/parser/ExpressionParser.java
@@ -23,7 +23,6 @@
  */
 package parser;
 
-import nbm.Nbm;
 import nbm.Nbm.Opcode;
 import scanner.Scanner;
 import scanner.Scanner.Symbol;
@@ -37,7 +36,6 @@ import symboltable.ValueOnStackOperand;
 public class ExpressionParser extends OperandExportingParser {
 
     private byte ror;
-    private Nbm.Opcode opCode;
     Operand op2;
 
     @Override
@@ -79,7 +77,6 @@ public class ExpressionParser extends OperandExportingParser {
     protected void parseOperator() {
         Scanner.Symbol currentMulOp = getScanner().getCurrentToken().getSymbol();
         parseSymbol(currentMulOp);
-        opCode = OperatorToOpCodeMap.getOpCode(currentMulOp);
         ror = OperatorToOpCodeMap.getOperand(currentMulOp);
     }
 
