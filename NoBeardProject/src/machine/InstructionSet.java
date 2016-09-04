2a39ab8690d0ab2aa6e6b49a0fac0a8577c180fe
Peter Bauer <p.bauer@htl-leonding.ac.at>
2016-09-05 01:02:00 +0200

    Removed superfluous size field and parameter from constructors.

--- NoBeardProject/src/machine/InstructionSet.java
+++ NoBeardProject/src/machine/InstructionSet.java
@@ -42,68 +42,80 @@ public class InstructionSet {
 
     public enum OperandType {
 
-        BYTE, HALFWORD
+        BYTE(1), HALFWORD(2);
+        
+        private final int size;
+        
+        OperandType(int size) {
+           this.size = size;
+       }
+        
+        public int getSizeInBytes() {
+            return size;
+        }
     };
 
     public enum Instruction {
 
-        NOP((byte) 0x00, (byte) 1, (cu) -> {
+        NOP((byte) 0x00, (cu) -> {
         }),
-        LIT((byte) 0x01, (byte) 3, OperandType.HALFWORD, (cu) -> {
+        LIT((byte) 0x01, OperandType.HALFWORD, (cu) -> {
             int n = cu.getLiteral();
             cu.getCallStack().push(n);
         }),
-        LA((byte) 0x02, (byte) 4, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
+        LA((byte) 0x02, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
             byte d = cu.getDisplacement();
             int a = cu.getAddress();
             int base = cu.getBaseFromDisplacement(d);
             cu.getCallStack().push(base + a);
         }),
-        LV((byte) 0x03, (byte) 4, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
+        LV((byte) 0x03, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
             byte d = cu.getDisplacement();
             int a = cu.getAddress();
             int base = cu.getBaseFromDisplacement(d);
             cu.getCallStack().push(cu.getDataMemory().loadWord(base + a));
         }),
-        LC((byte) 0x04, (byte) 4, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
+        LC((byte) 0x04, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
             byte d = cu.getDisplacement();
             int a = cu.getAddress();
             int base = cu.getBaseFromDisplacement(d);
             int c = cu.getDataMemory().loadByte(base + a);  // assigning byte to int does left padding to full word
             cu.getCallStack().push(c);
         }),
-        STO((byte) 0x07, (byte) 1, (cu) -> {
+        LVI((byte) 0x05, OperandType.BYTE, OperandType.HALFWORD, (cu) -> {
+        }),
+        STO((byte) 0x07, (cu) -> {
             int value = cu.getCallStack().pop();
             int a = cu.getCallStack().pop();
             cu.getDataMemory().storeWord(a, value);
         }),
-        STC((byte) 0x08, (byte) 1, (cu) -> {
+        STC((byte) 0x08, (cu) -> {
             byte value = (byte) cu.getCallStack().pop(); // remove upper significant bytes by casting
             int address = cu.getCallStack().pop();
             cu.getDataMemory().storeByte(address, value);
         }),
-        ASSN((byte) 0x0A, (byte) 1, (cu) -> {
+        ASSN((byte) 0x0A, (cu) -> {
             int n = cu.getCallStack().pop();
             int srcAddress = cu.getCallStack().pop();
             int dstAddress = cu.getCallStack().pop();
             cu.getDataMemory().copyBlock(srcAddress, dstAddress, n);
         }),
-        NEG((byte) 0x0B, (byte) 1, (cu) -> {
+        NEG((byte) 0x0B, (cu) -> {
             int x = cu.getCallStack().pop();
             cu.getCallStack().push(-x);
         }),
-        ADD((byte) 0x0C, (byte) 1, (cu) -> {
+        ADD((byte) 0x0C, (cu) -> {
             cu.getCallStack().push(cu.getCallStack().pop() + cu.getCallStack().pop());
         }),
-        SUB((byte) 0x0D, (byte) 1, (cu) -> {
+        SUB((byte) 0x0D, (cu) -> {
             int y = cu.getCallStack().pop();
             int x = cu.getCallStack().pop();
             cu.getCallStack().push(x - y);
         }),
-        MUL((byte) 0x0E, (byte) 1, (cu) -> {
+        MUL((byte) 0x0E, (cu) -> {
             cu.getCallStack().push(cu.getCallStack().pop() * cu.getCallStack().pop());
         }),
-        DIV((byte) 0x0F, (byte) 1, (cu) -> {
+        DIV((byte) 0x0F, (cu) -> {
             int y = cu.getCallStack().pop();
             int x = cu.getCallStack().pop();
             if (y != 0) {
@@ -112,7 +124,7 @@ public class InstructionSet {
                 cu.stopDueToDivisionByZero();
             }
         }),
-        MOD((byte) 0x10, (byte) 1, (cu) -> {
+        MOD((byte) 0x10, (cu) -> {
             int y = cu.getCallStack().pop();
             int x = cu.getCallStack().pop();
             if (y != 0) {
@@ -121,7 +133,7 @@ public class InstructionSet {
                 cu.stopDueToDivisionByZero();
             }
         }),
-        NOT((byte) 0x11, (byte) 1, (cu) -> {
+        NOT((byte) 0x11, (cu) -> {
             int x = cu.getCallStack().pop();
             if (x == 0) {
                 cu.getCallStack().push(1);
@@ -129,7 +141,7 @@ public class InstructionSet {
                 cu.getCallStack().push(0);
             }
         }),
-        REL((byte) 0x12, (byte) 2, OperandType.BYTE, (cu) -> {
+        REL((byte) 0x12, OperandType.BYTE, (cu) -> {
             int y = cu.getCallStack().pop();
             int x = cu.getCallStack().pop();
             byte relOp = cu.getType();
@@ -184,25 +196,25 @@ public class InstructionSet {
             }
             cu.getCallStack().push(valueToPush);
         }),
-        FJMP((byte) 0x16, (byte) 3, OperandType.HALFWORD, (cu) -> {
+        FJMP((byte) 0x16, OperandType.HALFWORD, (cu) -> {
             int newPc = cu.getAddress();
             int x = cu.getCallStack().pop();
             if (x == 0) {
                 cu.setPc(newPc);
             }
         }),
-        TJMP((byte) 0x17, (byte) 3, OperandType.HALFWORD, (cu) -> {
+        TJMP((byte) 0x17, OperandType.HALFWORD, (cu) -> {
             int newPc = cu.getAddress();
             int x = cu.getCallStack().pop();
             if (x == 1) {
                 cu.setPc(newPc);
             }
         }),
-        JMP((byte) 0x18, (byte) 3, OperandType.HALFWORD, (cu) -> {
+        JMP((byte) 0x18, OperandType.HALFWORD, (cu) -> {
             int newPc = cu.getAddress();
             cu.setPc(newPc);
         }),
-        OUT((byte) 0x1A, (byte) 2, OperandType.BYTE, (cu) -> {
+        OUT((byte) 0x1A, OperandType.BYTE, (cu) -> {
             byte type = cu.getType();
             switch (type) {
                 case 0:
@@ -221,37 +233,33 @@ public class InstructionSet {
                     cu.stopDueToOperandRangeError();
             }
         }),
-        INC((byte) 0x1D, (byte) 3, OperandType.HALFWORD, (cu) -> {
+        INC((byte) 0x1D, OperandType.HALFWORD, (cu) -> {
             int size = cu.getLiteral();
             int sp = cu.getCallStack().getStackPointer();
             cu.getCallStack().setStackPointer(sp + size);
         }),
-        HALT((byte) 0x1F, (byte) 1, (cu) -> {
+        HALT((byte) 0x1F, (cu) -> {
             cu.stopMachine();
         });
 
         private final byte id;
-        private final byte size;
         private final List<OperandType> operandTypes;
         private final InstructionImplementation implementation;
 
-        Instruction(byte id, byte size, InstructionImplementation implementation) {
+        Instruction(byte id, InstructionImplementation implementation) {
             this.id = id;
-            this.size = size;
             this.implementation = implementation;
             this.operandTypes = new ArrayList<>();
         }
-        
-        Instruction(byte id, byte size, OperandType operandType, InstructionImplementation implementation) {
+
+        Instruction(byte id, OperandType operandType, InstructionImplementation implementation) {
             this.id = id;
-            this.size = size;
             this.implementation = implementation;
             this.operandTypes = Arrays.asList(operandType);
         }
 
-        Instruction(byte id, byte size, OperandType operandType, OperandType operandType2, InstructionImplementation implementation) {
+        Instruction(byte id, OperandType operandType, OperandType operandType2, InstructionImplementation implementation) {
             this.id = id;
-            this.size = size;
             this.implementation = implementation;
             this.operandTypes = Arrays.asList(operandType, operandType2);
         }
@@ -261,13 +269,15 @@ public class InstructionSet {
         }
 
         byte getSize() {
-            return size;
+            int s = 0;
+            s = getOperandTypes().stream().map((t) -> t.getSizeInBytes()).reduce(s, Integer::sum) + 1;
+            return (byte) s;
         }
-        
+
         public boolean hasOperands() {
             return getOperandTypes().size() > 0;
         }
-        
+
         public List<OperandType> getOperandTypes() {
             return operandTypes;
         }
@@ -275,7 +285,7 @@ public class InstructionSet {
         void execute(ControlUnit controlUnit) {
             implementation.execute(controlUnit);
         }
-        
+
         @Override
         public String toString() {
             String s = super.toString();
