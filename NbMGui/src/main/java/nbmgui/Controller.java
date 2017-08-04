80d4e2424a62a878701e7add1b317d6a59b09583
Egon Manya <egon.manya@gmail.com>
2017-08-04 11:25:30 +0200

    Debugger optimization updated

--- NbMGui/src/main/java/nbmgui/Controller.java
+++ NbMGui/src/main/java/nbmgui/Controller.java
@@ -3,20 +3,19 @@ package nbmgui;
 import io.BinaryFile;
 import io.BinaryFileHandler;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import machine.NoBeardMachine;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
@@ -26,7 +25,8 @@ public class Controller {
     private BinaryFile objectFile;
     private Path path;
     private String input;
-    private VBox programData;
+    private HashMap<Integer, CheckBox> programDataMap;
+    private int lastProgramLine;
 
     @FXML
     private TextField inputView;
@@ -48,27 +48,34 @@ public class Controller {
     public void initialize() {
         machine = new NoBeardMachine(new FxInputDevice(this), new FxOutputDevice(this));
         semaphore = new Semaphore(0);
+        programDataMap = new HashMap<>();
         inputView.setDisable(true);
         startButton.setDisable(true);
         stepButton.setDisable(true);
         continueButton.setDisable(true);
         stopButton.setDisable(true);
         inputView.setOnKeyPressed(event -> {
-            if(event.getCode() == KeyCode.ENTER && inputView != null && !inputView.getText().isEmpty()){
+            if (event.getCode() == KeyCode.ENTER && inputView != null && !inputView.getText().isEmpty()) {
                 inputIsAvailable(inputView.getText());
             }
         });
     }
 
-    public Semaphore getSemaphore() { return semaphore; }
+    public Semaphore getSemaphore() {
+        return semaphore;
+    }
 
-    public String getInput() { return input; }
+    public String getInput() {
+        return input;
+    }
 
     public TextArea getOutputView() {
         return outputView;
     }
 
-    public TextField getInputView() { return inputView; }
+    public TextField getInputView() {
+        return inputView;
+    }
 
     @FXML
     void openFile(ActionEvent event) {
@@ -76,11 +83,10 @@ public class Controller {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("NoBeard-object Files", "*.no"));
         File selectedFile = fileChooser.showOpenDialog(null);
-        if (selectedFile != null){
+        if (selectedFile != null) {
             this.path = Paths.get(selectedFile.getAbsolutePath());
             prepareObjectFile();
-        }
-        else
+        } else
             outputView.appendText("Select a NoBeard object file\n");
     }
 
@@ -89,6 +95,7 @@ public class Controller {
         stepButton.setDisable(false);
         continueButton.setDisable(false);
         stopButton.setDisable(false);
+        lastProgramLine = -1;
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -112,6 +119,7 @@ public class Controller {
         machine.runUntilNextBreakpoint();
         highlightNextInstructionToBeExecuted();
     }
+
     @FXML
     void stopProgram(ActionEvent event) {
         stepButton.setDisable(true);
@@ -134,35 +142,35 @@ public class Controller {
     }
 
     private void fillProgramDataView(List<String> programDataList) {
-        programData = new VBox();
+        VBox programData = new VBox();
         for (String lineStr : programDataList) {
             CheckBox line = new CheckBox(lineStr);
             line.setPadding(new Insets(1));
-            line.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    if (event.getSource() instanceof CheckBox) {
-                        CheckBox breakpoint = (CheckBox) event.getSource();
-                        if (breakpoint.isSelected())
-                            machine.addBreakpoint(Integer.valueOf(breakpoint.getText().substring(2, 5)));
-                        else
-                            machine.removeBreakpoint(Integer.valueOf(breakpoint.getText().substring(2, 5)));
-                    }
+            line.setOnAction((event) -> {
+                if (event.getSource() instanceof CheckBox) {
+                    CheckBox breakpoint = (CheckBox) event.getSource();
+                    if (breakpoint.isSelected())
+                        machine.addBreakpoint(getAddressOfProgramLine(breakpoint.getText()));
+                    else
+                        machine.removeBreakpoint(getAddressOfProgramLine(breakpoint.getText()));
                 }
             });
             programData.getChildren().add(line);
+            programDataMap.put(getAddressOfProgramLine(line.getText()), line);
         }
         programDataView.setContent(programData);
     }
 
+    private int getAddressOfProgramLine(String line) {
+        return Integer.valueOf(line.substring(2, 5));
+    }
+
     private void highlightNextInstructionToBeExecuted() {
-        for (int i = 0; i < programData.getChildren().size(); i++) {
-            CheckBox checkBox = (CheckBox) programData.getChildren().get(i);
-            if (Integer.valueOf(checkBox.getText().substring(2, 5)) == machine.getPc())
-                ((CheckBox) programData.getChildren().get(i)).setStyle("-fx-background-color: #999999");
-            else
-                ((CheckBox) programData.getChildren().get(i)).setStyle("-fx-background-color: transparent");
-        }
+        if (programDataMap.containsKey(machine.getPc()))
+            programDataMap.get(machine.getPc()).setStyle("-fx-background-color: #999999");
+        if (lastProgramLine > -1)
+            programDataMap.get(lastProgramLine).setStyle("-fx-background-color: transparent");
+        lastProgramLine = machine.getPc();
     }
 
     private void inputIsAvailable(String providedInput) {
